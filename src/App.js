3495db15607091b9f22874cef9bf3bcd4cd34ae3
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-04-12 14:15:55 +0300

    Lesson 17

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Navbar/Nav";
 import Content from "./components/Content/content";
+import Dialogs from "./components/Dialogs/Dialogs";
+import classes from "./components/Content/content.module.css";
+
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <Header />
-            <Nav />
-            <Content />
-
+            <Header/>
+            <Nav/>
+            <div class='app-wrapper-content'>
+                <Dialogs/>
+            </div>
+            {/*<Content />*/}
         </div>
     )
 }
